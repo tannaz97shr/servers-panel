08cb199e1574ef11f8c33d2585fbf70b3ddbc3fe
tshirzadi <t_shirzadi@isc.co.ir>
2022-07-23 16:11:13 +0430

    init some states

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import FilterBox from "../components/filterBox/filterBox";
 
 const MainPage = () => {
+  const [minimumUptime, setMinimumUptime] = useState<number>();
+  const [maximumUptime, setMaximumUptime] = useState<number>();
   return (
     <div>
       <FilterBox />
